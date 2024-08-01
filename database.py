8abc4d6b1601a8dded6e3137b57d7e52abe8bb1e
Shivani2306 <129536470+Shivani2306@users.noreply.github.com>
2024-08-01 16:30:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -38,17 +38,3 @@ def get_db():
 
 
 
-
-
-
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-
-# DATABASE_URL = "sqlite:///./test.db"
-
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# from models import Base
-
-# Base.metadata.create_all(bind=engine)
